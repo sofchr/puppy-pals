cabe4e045a649e9f504d36a1a9078dcaac48631b
Sofie Christensen <sofiec510@gmail.com>
2023-08-03 17:00:21 -0400

    first three tabs of workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+  // console.log(puppies);
   return (
     <>
-     <div>
-
-     </div>
+      <div className="App">
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
